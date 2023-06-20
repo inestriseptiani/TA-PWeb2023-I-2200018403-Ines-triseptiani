9ba5eec74b69765a01f86eeaa641bd18b6d979bf
inestriseptiani <121371440+inestriseptiani@users.noreply.github.com>
2023-06-20 18:28:12 +0700

    Add files via upload

---
+++ PWeb2023-Tugas-09.2-I-2200018403-Ines Triseptiani.php
@@ -0,0 +1,11 @@
+<?php
+$a = 5;
+$b = 4;
+
+echo "$a == $b: " . ($a == $b);
+echo "<br>$a != $b: " . ($a != $b);
+echo "<br>$a > $b: " . ($a > $b);
+echo "<br>$a < $b: " . ($a < $b);
+echo "<br>($a == $b) && ($a > $b): " . (($a == $b) && ($a > $b));
+echo "<br>($a == $b) XOR ($a > $b): " . (($a == $b) XOR ($a > $b));
+?>
